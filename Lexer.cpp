210e5ec342d1be554395bedc1e1cf87f8fe4803e
johnsonlien <johnsonlien95@gmail.com>
2018-04-08 19:51:50 -0700

    Update Lexer.cpp

--- Lexer.cpp
+++ Lexer.cpp
@@ -1,9 +1,3 @@
-#include <iostream> 	//std::cout
-#include <string>	//string::string, string::length(), 
-#include <fstream>	//ofstrea::ofstream, ifstream::ifstream, open, close
-#include <vector>	//for vector, vector::size()
-#include <cctype>	//isdigit(), isalpha()
-#include <iomanip>	//std::setw() 
 #include "lexer.h"
 
 using namespace std;
